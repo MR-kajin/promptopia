cd6037b972ae00b9d5d7eb64117fe369a49d024e
Kajin-MSI <marco.raffaele@hotmail.com>
2023-05-14 11:43:17 +0200

    add edit-prompt feature

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
 
 const MyProfile = () => {
+    const router = useRouter();
 const { data: session } = useSession();
 
 const [posts, setPosts] = useState([]);
@@ -24,10 +25,10 @@ useEffect(() => {
         if(session?.user.id) fetchPosts();
     }, [])    
 
-const handleEdit = () => {
-    
+const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
 }
-const handleDelete = async () => {
+const handleDelete = async (post) => {
 
 }
 
